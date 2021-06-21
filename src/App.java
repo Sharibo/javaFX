a02f2e23ee049fdcca9e2b83e28af2a2d97d2adb
Sharibo <SystemofCybermirrorsWorld@gmail.com>
2021-06-22 00:48:23 +0300

    Second work with javaFX

--- src/App.java
+++ src/App.java
@@ -3,26 +3,41 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say, 'Hello World'");
+        Button btn = new Button("Say 'Hello'");
+        Label lbl1 = new Label("Input your name:");
+        Label lbl2 = new Label();
+        TextField txt = new TextField();
+
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");                
+                if ("".equals(txt.getText())) {
+                    lbl2.setText("Hello World!");
+                } else {
+                    lbl2.setText("Hello, " + txt.getText() + "!");
+                }
             }
             
         });
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        GridPane root = new GridPane();
+        root.add(lbl1, 0, 0);
+        root.add(txt, 1, 0);
+        root.add(btn, 1, 1);
+        root.add(lbl2, 1, 2);
+
+        root.setHgap(20);
+        root.setVgap(10);
 
         Scene scene = new Scene(root, 300, 250);
 
